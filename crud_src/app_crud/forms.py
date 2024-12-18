c7745d071c44c87d89c5e40be3d4da7bbd2914d0
lincolngadea <lincolngadea@gmail.com>
2024-12-18 00:22:57 -0300

    chore: inicia primeiras implementações para CRUD com DJONLA

--- crud_src/app_crud/forms.py
+++ crud_src/app_crud/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import User
 
-class UserForm(forms.modelForm):
+class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['nome','telefone','email']
--- crud_src/app_crud/views.py
+++ crud_src/app_crud/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from .forms import UserForm
+from .models import User
 
-# Create your views here.
+def index(request):
+
+    users = User.objects.all()
+
+    context = {
+        'users': users
+    }
+
+    return render(request, 'index.html', context)
+
+def create(request):
+
+    if request.method == 'GET':
+        form = UserForm()
+
+        context = {
+            'form': form,
+        }
+        return render(request, 'user/criar.html', context= context)
+    else:
+        form = UserForm(request.POST)
+        if form.is_valid():            
+            context ={
+                'nome': form.cleaned_data['nome'],
+                'telefone': form.cleaned_data['telefone'],
+                'email': form.cleaned_data['email']
+            }
+            return render(request, 'user/index.html', context= context)
+def change(request,user_id):
+    context = {
+        'id': user_id
+    }
+    return render(request, 'user/index.html', context= context)
